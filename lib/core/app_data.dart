2ce05371a3fe1618fda29b632926b67f01aae8b6
uyenlex <21020672@vnu.edu.vn>
2024-05-03 23:05:15 +0700

    update onboarding text

--- lib/core/app_data.dart
+++ lib/core/app_data.dart
@@ -3,8 +3,8 @@ class AppData {
   static const String description1 = 'VMEducation is a platform that provides information and learning materials for people interested in VME museum.';
   static const String title2 = 'Learn Anywhere, Anytime';
   static const String description2 = 'You can explore VMEducation courses on your mobile, tablet, or desktop.';
-  static const String title3 = 'View AR model in 3D';
-  static const String description3 = 'You can view the AR model in 3D and learn more about the subject.'; 
+  static const String title3 = 'View 3D models with AR technology';
+  static const String description3 = 'You can view the 3D model and learn more about the subject.'; 
 
   static const String skipText = 'Skip';
 }
